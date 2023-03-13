acfb526c7f8b8d8749ce5a3bebcf132e801c44cf
anazaryan <ashotnazaryan45@gmail.com>
2023-03-13 18:02:24 +0100

    test: test

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { MongoClientOptions, ServerApiVersion } from 'mongodb';
+import { ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 
 mongoose.Promise = global.Promise;
@@ -10,7 +10,7 @@ const password = process.env.MONGO_ATLAS_CLUSTER_PASSWORD;
 const db = process.env.MONGO_ATLAS_CLUSTER_DB;
 
 const connectToDatabase = async (): Promise<void> => {
-  const options: MongoClientOptions = { serverApi: ServerApiVersion.v1 };
+  const options: mongoose.ConnectOptions = { serverApi: ServerApiVersion.v1 };
 
   await mongoose.connect(`mongodb+srv://${user}:${password}@budget-management.ewiffuf.mongodb.net/${db}?retryWrites=true&w=majority`, options);
 };
