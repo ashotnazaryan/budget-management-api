f681e5dcdc55198b84dbd1593e5b773bd7ed5624
ashotnazaryan <ashotnazaryan45@outlook.com>
2023-03-13 16:56:35 +0100

    feat: init skeleton (#3)
    Co-authored-by: anazaryan <ashotnazaryan45@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000', 'https://app-budget-management.netlify.app']
 }));
 
 app.use('/api/summary', summaryRoute());
