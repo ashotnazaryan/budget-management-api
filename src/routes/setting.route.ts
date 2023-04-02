b1e0c02fee2784b6fa99a6dbf0d67c1d368f25bb
anazaryan <ashotnazaryan45@gmail.com>
2023-04-02 20:02:13 +0200

    feat: erase user data

--- src/routes/setting.route.ts
+++ src/routes/setting.route.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { CONFIG } from '../core/configs';
 import { isAuthenticated } from '../middleware';
-import { getSettings, addSetting } from '../services';
+import { getSettings, addSetting, deleteAllData } from '../services';
 
 const settingRoute = () => {
   const router = Router();
 
   router.get(CONFIG.routes.getSettings, isAuthenticated, getSettings);
   router.post(CONFIG.routes.addSetting, isAuthenticated, addSetting);
+  router.delete(CONFIG.routes.deleteAll, isAuthenticated, deleteAllData);
 
   return router;
 };
