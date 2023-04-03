4c453cff215e370da508054918287ab22c909cbd
anazaryan <ashotnazaryan45@gmail.com>
2023-04-03 20:16:22 +0200

    feat: edit category

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import { CONFIG } from '../core/configs';
 import { isAuthenticated } from '../middleware';
-import { getDefaultCategories, getCategories, createCategory } from '../services';
+import { getDefaultCategories, getCategories, getCategoryById, createCategory, editCategory } from '../services';
 
 const categoryRoute = () => {
   const router = Router();
 
   router.get(CONFIG.routes.getDefaultCategories, isAuthenticated, getDefaultCategories);
   router.get(CONFIG.routes.getCategories, isAuthenticated, getCategories);
+  router.get(CONFIG.routes.getCategoryById, isAuthenticated, getCategoryById);
   router.post(CONFIG.routes.createCategory, isAuthenticated, createCategory);
+  router.put(CONFIG.routes.editCategory, isAuthenticated, editCategory);
 
   return router;
 };
