b6d40d8cd19c60d33885e823046882a5f40e9e7c
anazaryan <ashotnazaryan45@gmail.com>
2023-04-26 13:36:37 +0200

    fix: edit transfer (for single transfer)

--- src/helpers/transfer.helpers.ts
+++ src/helpers/transfer.helpers.ts
@@ -1,14 +1,16 @@
 import { TransferDocument, TransferInput } from '../models';
 
 export const mapTransfers = (transfers: TransferDocument[], userId: string): TransferInput[] => {
-  return transfers.map((transfer) => {
-    return {
-      userId,
-      id: transfer.id,
-      fromAccount: transfer.fromAccount,
-      toAccount: transfer.toAccount,
-      amount: transfer.amount,
-      createdAt: transfer.createdAt
-    };
-  });
+  return transfers.map((transfer) => mapTransfer(transfer, userId));
+};
+
+export const mapTransfer = (transfer: TransferDocument, userId: string): TransferInput => {
+  return {
+    userId,
+    id: transfer.id,
+    fromAccount: transfer.fromAccount,
+    toAccount: transfer.toAccount,
+    amount: transfer.amount,
+    createdAt: transfer.createdAt
+  };
 };
