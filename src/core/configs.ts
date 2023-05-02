a817374b524f884730b6ad1e555a719dffcc97ab
ashotnazaryan <ashotnazaryan45@outlook.com>
2023-05-02 10:57:49 +0200

    test: test static spa (#79)
    Co-authored-by: anazaryan <ashotnazaryan45@gmail.com>

--- src/core/configs.ts
+++ src/core/configs.ts
@@ -62,7 +62,7 @@ export const CONFIG = {
 };
 
 export const GOOGLE_STRATEGY_OPTIONS: GoogleStrategyOptions = {
-  callbackURL: `/api/auth${CONFIG.routes.googleCallback}`,
+  callbackURL: `${CONFIG.clientURL}/api/auth${CONFIG.routes.googleCallback}`,
   scope: ['profile'],
   clientID: CONFIG.googleClientID,
   clientSecret: CONFIG.googleClientSecret,
