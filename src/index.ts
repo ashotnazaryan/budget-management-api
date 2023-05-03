147bb3b88cd18a9b6a2c0e8671c487ab0ce776a0
ashotnazaryan <ashotnazaryan45@outlook.com>
2023-05-03 10:10:07 +0200

    revert: revert static spa (#93)
    Co-authored-by: anazaryan <ashotnazaryan45@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,6 @@ const corsOptions = {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
-});
-
 app.use(
   cookieSession({
     name: CONFIG.cookieSessionSecretName,
